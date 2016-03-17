d01edda2596fcebc9b2343acdf3698e451bfbccf
Mike West <westmd23@gmail.com>
2016-03-16 22:12:13 -0400

    Response for question mark, all caps, and empty string. Need to fix errors for whatevers

--- bob.rb
+++ bob.rb
@@ -1,19 +1,26 @@
 class Bob
   def hey(remark)
-    if remark.upcase == remark && remark.downcase != remark
+    if remark.upcase == remark
       "Whoa, chill out!"
     elsif remark.end_with?("?")
       "Sure."
-    elsif remark.strip
-        "Fine. Be that way!"
+    elsif remark.strip.empty?
+      "Fine. Be that way!"
     else
-      "Whatever."
+      "Whatever"
     end
   end
 end
 
-
 # def test_shouting
 #   remark = 'WATCH OUT!'
 #   assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
+# end
+
+# def hey(remark)
+#   if remark.upcase == remark
+#     "Whoa, chill out!"
+#   else
+#     "Whatever"
+#   end
 # end
